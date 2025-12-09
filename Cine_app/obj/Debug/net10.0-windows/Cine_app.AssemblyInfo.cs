1a7bc55f196badb85854592d1ca8643142c0336a
Nixon Bolivar Cruz Hidalgo <nixonbcruzh@gmail.com>
2025-12-09 20:22:34 +0100

    chore: actualizar archivos de compilación y configuración del proyecto
    - Actualización de binarios compilados (Debug/Release)
    - Actualización de archivos de configuración de Visual Studio
    - Actualización de caché de compilación y metadatos del proyecto
    - Eliminación de referencia a submódulo obsoleto

--- Cine_app/obj/Debug/net10.0-windows/Cine_app.AssemblyInfo.cs
+++ Cine_app/obj/Debug/net10.0-windows/Cine_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cine_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feda2da76ad0011d34977d357f9b4225954b3097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b7572fa40ddc8cd37b9fdf1fc8d22cfb1364e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cine_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cine_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
